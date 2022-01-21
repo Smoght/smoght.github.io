7d6b1e64d944b048328578db5d3a949ccdea477c
John Romero <j.s.a.jr23@gmail.com>
2022-01-20 20:57:45 -0500

    correcciones de visivilidad en movile

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,13 @@ const menuIcon = document.querySelector('.icon-menu')
 const navigation = document.querySelector('.navigation')
 const portafolio =document.querySelector('.products')
 const links = document.querySelector('.nav-items')
+const typing = document.querySelector('.typing')
 menuIcon.onclick = function(){
     menuIcon.classList.toggle('active')
     navigation.classList.toggle('active')
     portafolio.classList.toggle('ocultar')
+    typing.classList.toggle('ocultar')
+
 }
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
